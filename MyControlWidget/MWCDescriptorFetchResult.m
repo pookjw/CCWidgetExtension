90b0491a9e1408484729bca72b0160e4f028c2a9
Jinwoo Kim <kidjinwoo@me.com>
2024-09-14 17:18:52 +0900

    no message

--- MyControlWidget/MWCDescriptorFetchResult.m
+++ MyControlWidget/MWCDescriptorFetchResult.m
@@ -7,6 +7,8 @@
 
 #import "MWCDescriptorFetchResult.h"
 
+// _TtC9WidgetKit21DescriptorFetchResult
+
 /*
  <?xml version="1.0" encoding="UTF-8"?>
  <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
